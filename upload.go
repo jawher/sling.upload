800c311837176b5662f6bf10fa9fe239f81c12c6
jawher <jawher.moussa@gmail.com>
2016-10-18 14:13:34 +0200

    v1, better but can still be enhanced

--- upload.go
+++ upload.go
@@ -15,60 +15,65 @@ var (
 	multipartFromBoundary = "SlingFormBoundary0amF3aGVy"
 )
 
-type multipartBodyProvider struct {
-	files       map[string]string
-	readers     map[string]namedReader
-	extraParams map[string]string
-}
-
-type namedReader struct {
-	fileName string
-	reader   io.ReadCloser
-}
-
-type part func(*multipartBodyProvider)
+type Part func(w *multipart.Writer) error
 
 // File creates a part from an existing file on disk. The name argument sets the part name, and path must point to an existing file on disk.
-func File(name string, path string) part {
-	return func(m *multipartBodyProvider) {
-		m.files[name] = path
+func File(name string, path string) Part {
+	return func(w *multipart.Writer) error {
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		fw, err := w.CreateFormFile(name, filepath.Base(path))
+		if err != nil {
+			return err
+		}
+		if _, err = io.Copy(fw, f); err != nil {
+			return err
+		}
+		return err
 	}
 }
 
 // Reader creates a part from a `io.Reader`. The name argument sets the part name, filename the uploaded file name and reader the content to be uploaded.
-func Reader(name string, fileName string, reader io.Reader) part {
+func Reader(name string, fileName string, reader io.Reader) Part {
 	rc, ok := reader.(io.ReadCloser)
 	if !ok {
 		rc = ioutil.NopCloser(reader)
 	}
 
-	return func(m *multipartBodyProvider) {
-		m.readers[name] = namedReader{
-			fileName: fileName,
-			reader:   rc,
+	return func(w *multipart.Writer) error {
+		defer rc.Close()
+		fw, err := w.CreateFormFile(name, fileName)
+		if err != nil {
+			return err
 		}
+		if _, err = io.Copy(fw, reader); err != nil {
+			return err
+		}
+		return nil
 	}
 }
 
 // Param creates a non-file part (regular key-value). The name argument sets the part name, and path must point to an existing file on disk.
-func Param(name string, value string) part {
-	return func(m *multipartBodyProvider) {
-		m.extraParams[name] = value
+func Param(name string, value string) Part {
+	return func(w *multipart.Writer) error {
+		fw, err := w.CreateFormField(name)
+		if err != nil {
+			return err
+		}
+		if _, err := fw.Write([]byte(value)); err != nil {
+			return err
+		}
+		return nil
 	}
 }
 
-func New(parts ...part) sling.BodyProvider {
-	m := &multipartBodyProvider{
-		files:       map[string]string{},
-		readers:     map[string]namedReader{},
-		extraParams: map[string]string{},
-	}
+type multipartBodyProvider []Part
 
-	for _, c := range parts {
-		c(m)
-	}
-
-	return m
+func New(parts ...Part) sling.BodyProvider {
+	return multipartBodyProvider(parts)
 }
 
 func (p multipartBodyProvider) ContentType() string {
@@ -80,38 +85,8 @@ func (p multipartBodyProvider) Body() (io.Reader, error) {
 	w := multipart.NewWriter(&body)
 	w.SetBoundary(multipartFromBoundary)
 
-	for paramName, filePath := range p.files {
-		f, err := os.Open(filePath)
-		if err != nil {
-			return nil, err
-		}
-		defer f.Close()
-		fw, err := w.CreateFormFile(paramName, filepath.Base(filePath))
-		if err != nil {
-			return nil, err
-		}
-		if _, err = io.Copy(fw, f); err != nil {
-			return nil, err
-		}
-	}
-
-	for paramName, namedReader := range p.readers {
-		defer namedReader.reader.Close()
-		fw, err := w.CreateFormFile(paramName, namedReader.fileName)
-		if err != nil {
-			return nil, err
-		}
-		if _, err = io.Copy(fw, namedReader.reader); err != nil {
-			return nil, err
-		}
-	}
-	// Add the other fields
-	for paramName, paramValue := range p.extraParams {
-		fw, err := w.CreateFormField(paramName)
-		if err != nil {
-			return nil, err
-		}
-		if _, err := fw.Write([]byte(paramValue)); err != nil {
+	for _, part := range p {
+		if err := part(w); err != nil {
 			return nil, err
 		}
 	}
--- upload_test.go
+++ upload_test.go
@@ -9,15 +9,17 @@ import (
 
 	"bytes"
 
+	"os"
+
 	"github.com/dghubble/sling"
 	"github.com/stretchr/testify/require"
 )
 
 func TestUpload(t *testing.T) {
 	b := New(
-		Param("param", "value"),
-		Reader("reader", "reader.txt", strings.NewReader("readervalue")),
 		File("file", "fixtures/file.txt"),
+		Reader("reader", "reader.txt", strings.NewReader("readervalue")),
+		Param("param", "value"),
 	)
 
 	require.Equal(t, "multipart/form-data; boundary=SlingFormBoundary0amF3aGVy", b.ContentType())
@@ -27,6 +29,8 @@ func TestUpload(t *testing.T) {
 
 	actualContent, err := ioutil.ReadAll(reader)
 	require.NoError(t, err)
+
+	ioutil.WriteFile("fixtures/actual.txt", actualContent, os.ModePerm)
 
 	expectedContent, err := ioutil.ReadFile("fixtures/expected.txt")
 	expectedContent = bytes.Replace(expectedContent, []byte("\n"), []byte("\r\n"), -1)
